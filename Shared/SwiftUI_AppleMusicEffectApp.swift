77c95658a42adc8a0322f81d802f1d35361b53a8
Lemonwaterpool <hschan345@gmail.com>
2020-06-24 17:40:49 +0900

    Initial Commit

---
+++ Shared/SwiftUI_AppleMusicEffectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_AppleMusicEffectApp.swift
+//  Shared
+//
+//  Created by H Chan on 2020/06/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_AppleMusicEffectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
